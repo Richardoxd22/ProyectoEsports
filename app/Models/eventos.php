2ead61733ed7a2f48abc5b09842d1f2812116c3a
Alejandro Vivanco <manuelinviv@gmail.com>
2020-11-17 21:52:01 -0500

    arreglo eventos

--- app/Models/eventos.php
+++ app/Models/eventos.php
@@ -7,13 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class eventos extends Model
 {
+
     use HasFactory;
     protected $table = 'eventos';
-<<<<<<< HEAD
-    protected $fillable = ['id_eventos', 'tituloevento', 'juegotorneo', 'informaciontorneo', 'discord', 'auth','eventofecha','eventoimagen'];
-=======
+
     protected $fillable = ['id_eventos', 'tituloevento', 'juegotorneo', 'informaciontorneo', 'discord', 'auth', 'eventofecha', 'eventoimagen'];
->>>>>>> fd451a082449d59f6ad10d6357b030aaa8e78ef1
+
+
     protected $primaryKey = 'id_eventos';
     public $incrementing = false;
     protected $keyType = 'int';
